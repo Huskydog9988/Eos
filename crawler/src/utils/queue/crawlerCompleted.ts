af5a8933062b17390a3121d98f5ed4f057ea894a
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-01-20 17:26:29 -0500

    Beginnings of a fix for bull
    Now not using unique jobs. Instead adding every job to mongo, and checking new links if they are already in mongo. Then update doc once done crawling.

--- crawler/src/utils/queue/crawlerCompleted.ts
+++ crawler/src/utils/queue/crawlerCompleted.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/node';
 import { Job } from 'bullmq';
 
-import { bulkAddToCrawlQueue, hashURI, logger } from '..';
+import { bulkAddToCrawlQueue, logger } from '..';
 import { Page } from '../../config';
 import { ParsedPage } from '../../@types/parsedPage';
 
@@ -11,7 +11,7 @@ import { ParsedPage } from '../../@types/parsedPage';
  * @param parsedPage
  * @returns
  */
-export const crawlerCompleted = (job: Job, parsedPage: ParsedPage) => {
+export const crawlerCompleted = async (job: Job, parsedPage: ParsedPage) => {
     // check if job failed to return proper data
     if (!parsedPage || parsedPage === undefined) {
         logger.warn(`Job ${job.id || job.name} failed to return proper data`);
@@ -21,24 +21,46 @@ export const crawlerCompleted = (job: Job, parsedPage: ParsedPage) => {
 
     logger.debug(`Parsed page ${parsedPage.url}`);
 
-    // add all links to the queue if not disabled
+    // add all links found to the queue if not disabled
     if (!process.env.NONEWJOBS) bulkAddToCrawlQueue(parsedPage.links);
 
-    const page = {
-        url: parsedPage.url,
-        hash: job.id || hashURI(parsedPage.url),
-        title: parsedPage.title,
-        description: parsedPage.description,
-        keywords: parsedPage.keywords,
-    };
+    // const page = {
+    //     url: parsedPage.url,
+    //     hash: job.id || hashURI(parsedPage.url),
+    //     title: parsedPage.title,
+    //     description: parsedPage.description,
+    //     keywords: parsedPage.keywords,
+    // };
 
-    // save page to mongo
-    Page.create(page)
-        .catch((error) => {
-            console.error(error);
-            Sentry.captureException(error);
-        })
-        .then(() => {
-            logger.debug(`Saved url ${parsedPage.url}`);
-        });
+    // find document created when first added to the queue
+    const doc = await Page.findOne({ url: parsedPage.url });
+
+    // check if it was found
+    if (doc === null || !(doc instanceof Page)) {
+        const message = `Couldn't find page ${parsedPage.url} in mongo to update`;
+        logger.error(message);
+        Sentry.captureMessage(message);
+
+        return;
+    }
+
+    // update page with info from latest crawl
+    doc.url = parsedPage.url;
+    doc.title = parsedPage.title;
+    doc.description = parsedPage.description;
+    doc.keywords = parsedPage.keywords;
+    doc.updated = Date.now();
+
+    // save
+    await doc.save();
+
+    // // save page to mongo
+    // Page.create(page)
+    //     .catch((error) => {
+    //         console.error(error);
+    //         Sentry.captureException(error);
+    //     })
+    //     .then(() => {
+    //         logger.debug(`Saved url ${parsedPage.url}`);
+    //     });
 };
