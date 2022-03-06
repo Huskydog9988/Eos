450107c1afab4751f3930545e083ae2fd8aa17f6
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2022-03-05 19:18:52 -0500

    auto log rejections and exceptions

--- crawler/src/utils/logger.ts
+++ crawler/src/utils/logger.ts
@@ -18,13 +18,37 @@ const dailyRotatErrorFile = new DailyRotateFile({
     level: 'warn',
 });
 
+const dailyRotatExceptionFile = new DailyRotateFile({
+    filename: './logs/%DATE%-Error.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    level: 'warn',
+});
+
+const dailyRotatRejectionsFile = new DailyRotateFile({
+    filename: './logs/%DATE%-Error.log',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    level: 'warn',
+});
+
 /**
  * Logger
  */
 export const logger = winston.createLogger({
     exitOnError: false,
     transports: [dailyRotateFile, dailyRotatErrorFile],
-    format: winston.format.printf((log) => `[${log.level.toUpperCase()}] - ${log.message}`),
+    exceptionHandlers: [dailyRotatExceptionFile],
+    rejectionHandlers: [dailyRotatRejectionsFile],
+    format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
 
     // format: winston.format.combine(
     //   winston.format.colorize(),
